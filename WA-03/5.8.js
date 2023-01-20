f96e296e4b1eca19f1bc740e63491f716dc31e4c
mbukhman <mbukhman@gmail.com>
2023-01-21 00:28:45 +0400

    check for empty arguments

--- WA-03/5.8.js
+++ WA-03/5.8.js
@@ -3,10 +3,14 @@
 // String will never be empty and you do not need to account for different data types.
 
 const shortestWord = (str) =>
-    str.split(" ").reduce((a, b) => (a.length > b.length ? a : b));
+    str && str.split(" ").reduce((a, b) => (a.length > b.length ? a : b));
 
 // Tests
 let res;
+res = shortestWord();
+console.log(res);
+res = shortestWord("");
+console.log(res);
 res = shortestWord("Hello is that possible oy");
 console.log(res);
 res = shortestWord("bcd a sf!4");
