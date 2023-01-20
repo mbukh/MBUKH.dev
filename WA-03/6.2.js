8212910e7c984420b0b61575f8272d032e653e5b
mbukhman <mbukhman@gmail.com>
2023-01-21 03:08:25 +0400

    match version done

--- WA-03/6.2.js
+++ WA-03/6.2.js
@@ -2,20 +2,27 @@
 const countDuplicates = (str) =>
     str &&
     /^[a-zA-Z0-9]+$/.test(str) &&
-    str
-        .toLowerCase()
+    str?.toLowerCase()
         .split("")
         .reduce(
-            (storage, letter) =>
-                Object.assign({}, storage, {
-                    [letter]: (storage[letter] || 0) + 1,
-                    dupCount:
-                        storage[letter] === 1
-                            ? storage.dupCount + 1
-                            : storage.dupCount,
+            (acc, ch) =>
+                Object.assign({}, acc, {
+                    [ch]: (acc[ch] || 0) + 1,
+                    dCnt: acc[ch] === 1 ? acc.dCnt + 1 : acc.dCnt,
                 }),
-            { dupCount: 0 }
-        ).dupCount;
+            { dCnt: 0 }
+        ).dCnt;
+
+// Easier String.match implementation
+// ? before . means optional chaining  operator
+// it returns undefined if null or undefined stands before it
+const countDuplicates2 = (str) =>
+    [...new Set(str?.toLowerCase())]
+    .reduce(
+        (cnt, ch) =>
+            str.match(new RegExp(ch, "gi"))?.length > 1 ? ++cnt : cnt,
+        0
+    );
 
 // Tests
 let res;
@@ -27,3 +34,13 @@ res = countDuplicates("indivisibility"); //=
 res = countDuplicates("Indivisibilities"); //=
 res = countDuplicates("aA11"); //=
 res = countDuplicates("ABBA"); //=
+
+let res2;
+res2 = countDuplicates2(); //=
+res2 = countDuplicates2("abcde"); //=
+res2 = countDuplicates2("aabbcde"); //=
+res2 = countDuplicates2("aabBcde"); //=
+res2 = countDuplicates2("indivisibility"); //=
+res2 = countDuplicates2("Indivisibilities"); //=
+res2 = countDuplicates2("aA11"); //=
+res2 = countDuplicates2("ABBA"); //=
