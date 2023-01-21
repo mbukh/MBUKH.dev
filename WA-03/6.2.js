bbad3c8778f17f42ef0fb1148ed96cdc34c74c7f
mbukhman <mbukhman@gmail.com>
2023-01-21 18:43:43 +0400

    fix string argument check

--- WA-03/6.2.js
+++ WA-03/6.2.js
@@ -1,46 +1,68 @@
 // reduce via object implementation
+// ? before . means optional chaining  operator
+// it returns undefined if null or undefined stands before it
 const countDuplicates = (str) =>
     str &&
+    typeof str === "string" &&
     /^[a-zA-Z0-9]+$/.test(str) &&
-    str?.toLowerCase()
+    str
+        ?.toLowerCase()
         .split("")
         .reduce(
             (acc, ch) =>
                 Object.assign({}, acc, {
                     [ch]: (acc[ch] || 0) + 1,
+                    // check for 1 cuz acc is still not updated
                     dCnt: acc[ch] === 1 ? acc.dCnt + 1 : acc.dCnt,
                 }),
             { dCnt: 0 }
         ).dCnt;
 
 // Easier String.match implementation
-// ? before . means optional chaining  operator
-// it returns undefined if null or undefined stands before it
 const countDuplicates2 = (str) =>
-    [...new Set(str?.toLowerCase())]
-    .reduce(
-        (cnt, ch) =>
-            str.match(new RegExp(ch, "gi"))?.length > 1 ? ++cnt : cnt,
+    typeof str === "string" &&
+    [...new Set(str.toLowerCase())].reduce(
+        (cnt, ch) => (str.match(new RegExp(ch, "gi")).length > 1 ? ++cnt : cnt),
         0
     );
 
 // Tests
 let res;
 res = countDuplicates(); //=
+console.log(res);
+res = countDuplicates(123); //=
+console.log(res);
 res = countDuplicates("abcde"); //=
+console.log(res);
 res = countDuplicates("aabbcde"); //=
+console.log(res);
 res = countDuplicates("aabBcde"); //=
+console.log(res);
 res = countDuplicates("indivisibility"); //=
+console.log(res);
 res = countDuplicates("Indivisibilities"); //=
+console.log(res);
 res = countDuplicates("aA11"); //=
+console.log(res);
 res = countDuplicates("ABBA"); //=
+console.log(res);
 
 let res2;
 res2 = countDuplicates2(); //=
+console.log(res);
+res2 = countDuplicates2(123); //=
+console.log(res);
 res2 = countDuplicates2("abcde"); //=
+console.log(res);
 res2 = countDuplicates2("aabbcde"); //=
+console.log(res);
 res2 = countDuplicates2("aabBcde"); //=
+console.log(res);
 res2 = countDuplicates2("indivisibility"); //=
+console.log(res);
 res2 = countDuplicates2("Indivisibilities"); //=
+console.log(res);
 res2 = countDuplicates2("aA11"); //=
+console.log(res);
 res2 = countDuplicates2("ABBA"); //=
+console.log(res);
--- WA-03/6.3.js
+++ WA-03/6.3.js
@@ -1,5 +1,5 @@
 const longest = (s1, s2) => {
-    if (!(s1?.length && s2?.length)) return "two string are required";
+    if (!s1?.length || !s2?.length) return "two string are required";
     return [...new Set(s1 + s2)]
         .sort(Intl.Collator().compare) // international case insensitive
         .join("");
@@ -16,9 +16,10 @@ console.log(res);
 res = longest("abc", 123); //=
 console.log(res);
 
+console.log(res);
 res = longest("abc", "def"); //=
 console.log(res);
-res = longest("cba", "cbafed"); //=
+res = longest("cba", "cbfed"); //=
 console.log(res);
 res = longest("ЕдГ", "вбА"); //=
 console.log(res);
